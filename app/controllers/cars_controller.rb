db470478002e3c0c7243274c6cd968438e7d558c
navinspm <navinspm@gmail.com>
2014-09-28 12:05:44 +0530

    modified cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,28 +1,4 @@
 class CarsController < ApplicationController
 
-  def index
-    @cars = Car.all
-  end
-
-  def show
-     @car = Car.find(params[:id])
-    end
-  end
-
-  def create
-    @car = Car.new(car_params)
-
-    respond_to do |format|
-      if @car.save
-        format.html { redirect_to @car, notice: 'Car was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-
-    private
-    def car_params
-      params.require(:car).permit(:name,:price, :description,:image)
-    end
   end
 
